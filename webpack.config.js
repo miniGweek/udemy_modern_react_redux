7403256185b396d79476f7f07823b357f0adc1cd
miniGweek <dedlok@gmail.com>
2017-06-04 13:05:30 +1000

    First Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 module.exports = {
   entry: [
     './src/index.js'
@@ -22,5 +23,25 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+    new BrowserSyncPlugin(
+      // BrowserSync options 
+      {
+        // browse to http://localhost:3000/ during development 
+        host: 'localhost',
+        port: 8080,
+        // proxy the Webpack Dev Server endpoint 
+        // (which should be serving on http://localhost:3100/) 
+        // through BrowserSync 
+        proxy: 'http://localhost:3100/'
+      },
+      // plugin options 
+      {
+        // prevent BrowserSync from reloading the page 
+        // and let Webpack Dev Server take care of this 
+        reload: false
+      }
+    )
+  ]
 };
