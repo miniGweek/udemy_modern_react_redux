7bf5af38bb0959263509b999f1cb471c179ade91
miniGweek <dedlok@gmail.com>
2017-06-05 19:50:32 +1000

    binding to event raised on the input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,8 +4,12 @@ import React,{Component} from 'react'
 class SearchBar extends Component{
     render(){
         return (
-            <input />
+            <input onChange={ this.onInputChange}/>
         );
+    }
+
+    onInputChange(event){
+        console.log(event);
     }
 }
 
