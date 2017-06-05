de759bcb939902821e69a6ea95f587c7a0a6c33e
miniGweek <dedlok@gmail.com>
2017-06-05 19:51:18 +1000

    event as fat arrow function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,12 +4,8 @@ import React,{Component} from 'react'
 class SearchBar extends Component{
     render(){
         return (
-            <input onChange={ this.onInputChange}/>
+            <input onChange={ event => console.log(event.target.value)}/>
         );
-    }
-
-    onInputChange(event){
-        console.log(event);
     }
 }
 
