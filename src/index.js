d47a2e5d1239b2495013e1c5e9f5e53a6eb5504b
miniGweek <dedlok@gmail.com>
2017-06-05 21:24:35 +1000

    ajax call and setting data on state using callback and setstate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY='AIzaSyANuCesK5DCpO0NKqdECVrtW0Ra7W62JlI';
 
-const App = () =>{
-    return (
-    <div>
-        <SearchBar/>
-    </div>);
+class App extends Component{
+    constructor(props){
+        super(props);
+
+    YTSearch({key:API_KEY, term:'surfboards'},
+     videos => { 
+         this.setState(videos)
+        });
+
+    }
+
+
+    render(){
+        return (
+                <div>
+                    <SearchBar/>
+                </div>);
+          }       
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
