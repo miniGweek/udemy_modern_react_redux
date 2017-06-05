5ecab3af6f05230801cae1dd1db7828d9fcd5e78
miniGweek <dedlok@gmail.com>
2017-06-05 20:56:41 +1000

    using state binding state of an input field to a  div text content

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,16 @@ import React,{Component} from 'react'
 
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {'term' : null}
+    }
     render(){
         return (
-            <input onChange={ event => console.log(event.target.value)}/>
+            <div>
+                <input onChange={ event =>this.setState({'term':event.target.value})}/>
+                {this.state.term}
+            </div>
         );
     }
 }
